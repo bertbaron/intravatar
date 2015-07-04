0407d1772cc6725d9f63f8bc298798e8351b1c15
Bert Baron <bertusbaron@gmail.com>
2015-07-04 21:35:57 +0200

    Very basic implementation that does serve avatars, but does not nicely handling missing avatars.

--- main.go
+++ main.go
@@ -2,20 +2,38 @@ package main
 
 import (
 	"flag"
-	"github.com/vharitonsky/iniflags"
 	"fmt"
+	"github.com/vharitonsky/iniflags"
+	"io"
+	"log"
 	"net/http"
+	"os"
 	"regexp"
 )
 
 var (
-    dataDir = flag.String("data", "data", "Path to data files relative to current working dir. Default is 'data'.")
-    port = flag.Int("port", 8080, "Webserver port number. Default = 8080")
+	dataDir = flag.String("data", "data", "Path to data files relative to current working dir.")
+	port    = flag.Int("port", 8080, "Webserver port number.")
 )
 
+func writeFile(hash string, w io.Writer) error {
+	filename := fmt.Sprintf("%s/%s", *dataDir, hash)
+    
+	file, err := os.Open(filename)
+	if err != nil {
+		log.Printf("Error reading file: %v", err)
+		return err
+	}
+
+	defer file.Close()
+	
+	_, e := io.Copy(w, file)
+	return e
+}
+
 func avatarHandler(w http.ResponseWriter, r *http.Request, title string) {
-	fmt.Println("Handling avatar request", title, *r)
-	fmt.Fprintf(w, "<p>Avatar</p>")
+	log.Print("Handling avatar request", title)
+	writeFile(title, w)
 }
 
 var validPath = regexp.MustCompile("^/(avatar)/([a-zA-Z0-9]+)$")
@@ -33,9 +51,10 @@ func makeHandler(fn func(http.ResponseWriter, *http.Request, string)) http.Handl
 
 func main() {
 	iniflags.Parse()
-	fmt.Printf("data dir = %s\n", *dataDir)
+	log.Printf("data dir = %s\n", *dataDir)
 	address := fmt.Sprintf(":%d", *port)
-	
+	log.Printf("Listening on %s\n", address)
+
 	http.HandleFunc("/avatar/", makeHandler(avatarHandler))
 	x := http.ListenAndServe(address, nil)
 	fmt.Println("Result: ", x)
