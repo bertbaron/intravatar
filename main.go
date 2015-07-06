ae27d8c26eaca34fdb4673bea59536e1ab943b49
Bert Baron <bertusbaron@gmail.com>
2015-07-06 20:02:01 +0200

    Using the word 'remote' instead of 'fallback'. Fallback may cause confusion with the term 'default', while remote seems to make sense in a proxy context.

--- main.go
+++ main.go
@@ -14,17 +14,26 @@ import (
 )
 
 var (
-	dataDir  = flag.String("data", "data", "Path to data files relative to current working dir.")
-	port     = flag.Int("port", 8080, "Webserver port number.")
-	fallback = flag.String("fallback", "http://gravatar.com/avatar", "Fallback on this gravatar-compatible avatar service if no avatar is found, use 'none' for no fallback")
-	dflt     = flag.String("default", "resources/mm", "Default avatar. Use 'fallback' to use the default of the fallback service, or 'fallback:<option>' to use a builtin default. For example: 'fallback:monsterid'. This is passes as 'd=monsterid' to the fallback service. See https://nl.gravatar.com/site/implement/images/.")
+	dataDir = flag.String("data", "data", "Path to data files relative to current working dir.")
+	port    = flag.Int("port", 8080, "Webserver port number.")
+	remote  = flag.String("remote", "http://gravatar.com/avatar", "Use this gravatar-compatible avatar service if "+
+		"no avatar is found, use 'none' for no remote.")
+	dflt = flag.String("default", "resources/mm", "Default avatar. Use 'remote' to use the default of the remote "+
+		"service, or 'remote:<option>' to use a builtin default. For example: 'remote:monsterid'. This is passes as "+
+		"'?d=monsterid' to the remote service. See https://nl.gravatar.com/site/implement/images/.")
 )
 
 var (
-	defaultImage    = "resources/mm"
-	fallbackUrl     = ""
-	fallbackDefault = ""
+	defaultImage  = "resources/mm"
+	remoteUrl     = ""
+	remoteDefault = ""
 )
+
+type Avatar struct {
+	mimetype string
+	size     int
+	data     []byte
+}
 
 func createPath(hash string) string {
 	return fmt.Sprintf("%s/%s", *dataDir, hash)
@@ -33,16 +42,16 @@ func createPath(hash string) string {
 func loadImage(hash string, w io.Writer) error {
 	filename := createPath(hash)
 	if _, err := os.Stat(filename); os.IsNotExist(err) {
-		if fallbackUrl == "" {
+		if remoteUrl == "" {
 			log.Printf("%s does not exist, falling back to default", filename)
 			filename = defaultImage
 		} else {
-			log.Printf("%s does not exist, redirecting to %s", filename, fallbackUrl)
+			log.Printf("%s does not exist, redirecting to %s", filename, remoteUrl)
 			options := ""
-			if fallbackDefault != "" {
-				options = fmt.Sprintf("?d=%s", fallbackDefault)
+			if remoteDefault != "" {
+				options = fmt.Sprintf("?d=%s", remoteDefault)
 			}
-			remote := fmt.Sprintf("%s/%s%s", fallbackUrl, hash, options)
+			remote := fmt.Sprintf("%s/%s%s", remoteUrl, hash, options)
 			resp, err2 := http.Get(remote)
 			if err2 != nil {
 				log.Printf("Remote lookup of %s failed with error: %s", remote, err2)
@@ -86,7 +95,7 @@ func renderTemplate(w http.ResponseWriter, tmpl string, data interface{}) {
 }
 
 func uploadHandler(w http.ResponseWriter, r *http.Request, title string) {
-	renderTemplate(w, "upload", map[string]string{"Title" : "Upload your avatar"})
+	renderTemplate(w, "upload", map[string]string{"Title": "Upload your avatar"})
 }
 
 func saveHandler(w http.ResponseWriter, r *http.Request, title string) {
@@ -98,7 +107,7 @@ func saveHandler(w http.ResponseWriter, r *http.Request, title string) {
 		fmt.Fprintf(w, "<p>Please chooce a file to upload</p>")
 		return
 	}
-	
+
 	h := md5.New()
 	io.WriteString(h, email) // FIXME trim and toLowerCase!
 	hash := fmt.Sprintf("%x", h.Sum(nil))
@@ -112,8 +121,8 @@ func saveHandler(w http.ResponseWriter, r *http.Request, title string) {
 	}
 	defer f.Close()
 	io.Copy(f, file)
-	
-	renderTemplate(w, "view", map[string]string{"Avatar" : fmt.Sprintf("/avatar/%s", hash)})
+
+	renderTemplate(w, "view", map[string]string{"Avatar": fmt.Sprintf("/avatar/%s", hash)})
 }
 
 var validPath = regexp.MustCompile("^/(avatar|upload)/([a-zA-Z0-9]+)(/.*)?$")
@@ -140,22 +149,22 @@ func main() {
 	log.Printf("data dir = %s\n", *dataDir)
 	address := fmt.Sprintf(":%d", *port)
 
-	if *fallback == "none" {
-		fallbackUrl = ""
+	if *remote == "none" {
+		remoteUrl = ""
 	} else {
-		fallbackUrl = *fallback
-		log.Printf("Missig avatars will be redirected to %s", fallbackUrl)
+		remoteUrl = *remote
+		log.Printf("Missig avatars will be redirected to %s", remoteUrl)
 	}
 
 	if *dflt == "fallback" {
-		log.Printf("Default image will be provided by the fallback service if configured")
-		fallbackDefault = ""
+		log.Printf("Default image will be provided by the remote service if configured")
+		remoteDefault = ""
 	} else if builtin := fallbackDefaultPattern.FindStringSubmatch(*dflt); builtin != nil {
-		fallbackDefault = builtin[1]
-		log.Printf("Default image will be provided by the fallback service using '%s' if configured", fallbackDefault)
+		remoteDefault = builtin[1]
+		log.Printf("Default image will be provided by the remote service using '?d=%s' if a remote is configured", remoteDefault)
 	} else {
 		defaultImage = *dflt
-		fallbackDefault = "404"
+		remoteDefault = "404"
 		log.Printf("Using %s as default image", defaultImage)
 	}
 
