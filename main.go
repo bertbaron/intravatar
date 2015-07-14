09608dbb0da44870b3ce34bab1eab4bc596cb407
bert <bertusbaron@gmail.com>
2015-07-14 07:00:52 +0200

    Moved code for downloading/retrieving avatars to download.go

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"bytes"
 	"crypto/md5"
 	"flag"
 	"fmt"
@@ -13,7 +12,6 @@ import (
 	"net/http"
 	"os"
 	"regexp"
-	"strconv"
 	"strings"
 	"time"
 )
@@ -48,66 +46,6 @@ const (
 	configFile = "config.ini"
 )
 
-type Request struct {
-	hash string
-	size int
-}
-
-func readFromFile(filename string, size int) *Avatar {
-	file, err := os.Open(filename)
-	if err != nil {
-		log.Printf("Error reading file: %v", err)
-		return nil
-	}
-	defer file.Close()
-	avatar := readImage(file)
-	err = scale(avatar, size)
-	if err != nil {
-		log.Printf("Could not scale image: %v", err)
-		return nil // don't return the image, if we can't scale it it is probably corrupt
-	}
-
-	avatar.cacheControl = "max-age=300"
-	if info, e := os.Stat(filename); e == nil {
-		timestamp := info.ModTime().UTC()
-		avatar.lastModified = timestamp.Format(http.TimeFormat)
-	} else {
-		avatar.lastModified = "Sat, 1 Jan 2000 12:00:00 GMT"
-	}
-	return avatar
-}
-
-func retrieveFromLocal(request Request) *Avatar {
-	return readFromFile(createAvatarPath(request.hash), request.size)
-}
-
-// Retrieves the avatar from the remote service, returning nil if there is no avatar or it could not be retrieved
-func retrieveFromRemote(request Request) *Avatar {
-	if remoteUrl == "" {
-		return nil
-	}
-	options := fmt.Sprintf("s=%d", request.size)
-	if remoteDefault != "" {
-		options += "&d=" + remoteDefault
-	}
-	remote := remoteUrl + "/" + request.hash + "?" + options
-	log.Printf("Retrieving from: %s", remote)
-	resp, err2 := http.Get(remote)
-	if err2 != nil {
-		log.Printf("Remote lookup of %s failed with error: %s", remote, err2)
-		return nil
-	}
-	if resp.StatusCode == 404 {
-		log.Printf("Avatar not found on remote")
-		return nil
-	}
-	avatar := readImage(resp.Body)
-	avatar.size = request.size // assume image is scaled by remote service
-	avatar.cacheControl = resp.Header.Get("Cache-Control")
-	avatar.lastModified = resp.Header.Get("Last-Modified")
-	return avatar
-}
-
 func createAvatarPath(hash string) string {
 	return fmt.Sprintf("%s/avatars/%s", *dataDir, hash)
 }
@@ -124,58 +62,6 @@ func setHeaderField(w http.ResponseWriter, key string, value string) {
 	if value != "" {
 		w.Header().Set(key, value)
 	}
-}
-
-func writeAvatarResult(w http.ResponseWriter, avatar *Avatar) {
-	setHeaderField(w, "Last-Modified", avatar.lastModified)
-	setHeaderField(w, "Cache-Control", avatar.cacheControl)
-	b := bytes.NewBuffer(avatar.data)
-	_, err := io.Copy(w, b)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-	}
-}
-
-func retrieveImage(request Request, w http.ResponseWriter, r *http.Request) *Avatar {
-	avatar := retrieveFromLocal(request)
-	if avatar == nil {
-		avatar = retrieveFromRemote(request)
-	}
-	if avatar == nil {
-		avatar = readFromFile(defaultImage, request.size)
-	}
-	if avatar == nil {
-		avatar = readFromFile("resources/mm", request.size)
-	}
-	return avatar
-}
-
-func loadImage(request Request, w http.ResponseWriter, r *http.Request) {
-	log.Printf("Loading image: %v", request)
-	avatar := retrieveImage(request, w, r)
-	if avatar == nil {
-		http.NotFound(w, r)
-	} else {
-		writeAvatarResult(w, avatar)
-	}
-}
-
-func avatarHandler(w http.ResponseWriter, r *http.Request, title string) {
-	r.ParseForm()
-	sizeParam := r.FormValue("s")
-	size := 80
-	if sizeParam != "" {
-		if s, err := strconv.Atoi(sizeParam); err == nil {
-			size = s
-			if size > maxSize {
-				size = maxSize
-			}
-			if size < minSize {
-				size = minSize
-			}
-		}
-	}
-	loadImage(Request{hash: title, size: size}, w, r)
 }
 
 func renderTemplate(w http.ResponseWriter, tmpl string, data interface{}) {
